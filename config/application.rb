ac9fad18e8b039ec27cee706ff9e7b3f58f99ead
Ilya Rogozin <ilya.rogozin@gmail.com>
2011-11-25 14:32:18 +0400

    настройки сайта

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
+require File.expand_path("../settings", __FILE__)
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
---
+++ config/settings.rb
@@ -0,0 +1,21 @@
+module Settings
+  class << self
+    def giftpoisk?
+      true
+    end  
+
+    def giftb2b?
+      false
+    end  
+    
+    def site_name
+      "giftpoisk.ru"
+    end    
+    
+    def site_id
+      1
+    end    
+    
+    
+  end
+end
